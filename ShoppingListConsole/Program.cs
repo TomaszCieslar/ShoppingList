0a5df54260a30272046a8f4a81917bd14b891ae9
Tomasz Cieslar <tomasz.cieslar@capgemini.com>
2021-05-03 11:41:27 +0200

    Initial change

--- ShoppingListConsole/Program.cs
+++ ShoppingListConsole/Program.cs
@@ -6,7 +6,7 @@ namespace ShoppingListConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World1!");
         }
     }
 }
