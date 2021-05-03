a87035633faeec28517819bd5feef9e794f31f49
Tomasz Cieslar <tomasz.cieslar@capgemini.com>
2021-05-03 11:37:05 +0200

    Add project files.

---
+++ ShoppingListConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ShoppingListConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
