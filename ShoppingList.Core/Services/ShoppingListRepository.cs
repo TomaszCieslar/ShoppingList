ac15ff52ac2aef414e59b88ef2018235e5b830f8
Tomasz Cieslar <tomasz.cieslar@capgemini.com>
2021-05-04 08:42:36 +0200

    Onion architecture - basic

--- ShoppingList.Core/Services/ShoppingListRepository.cs
+++ ShoppingList.Core/Services/ShoppingListRepository.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace ShoppingListConsole
+namespace ShoppingList.Core
 {
     public class ShoppingListRepository : IShoppingListRepository
     {
